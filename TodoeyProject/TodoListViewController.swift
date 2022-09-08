170d3fa356215f8e3826521a247a5af4ca734473
YE002 <YE002@gadgetzones-MacBook-Air.local>
2022-09-08 15:15:02 +0530

    Add New Item

--- TodoeyProject/TodoListViewController.swift
+++ TodoeyProject/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Bike", "Find Rider", "Start Journey"]
+    var itemArray = ["Find Bike", "Find Rider", "Start Journey"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,22 +30,41 @@ class TodoListViewController: UITableViewController {
 //MARK: - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // print row number which row you have selected
-          //print(indexPath.row)
-        // print selected row title
-          //print(itemArray[indexPath.row])
-        // when selected row added checked mark i.e accessory type
-           //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
-        // when selecting row grey colour in table row is disappear
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+//MARK: - Add New Items Bar Button
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will Happen Once the user clicks the Add Item Button on our UIAlert
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+            // after appending data in array we need to relaod otherwise not shown data
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            // Add button click excute this one then click Add Item
+            print(alertTextField.text)
+            print("now")
+        }
+        // Add Item Button Shows
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
